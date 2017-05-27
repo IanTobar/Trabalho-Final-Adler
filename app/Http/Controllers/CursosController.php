63ebee9ce96ba7ca3ff7172191d6d56e7c4300ce
Unknown <ruanbento@gmail.com>
2017-05-27 19:09:25 -0300

    Adicionado funções necessárias para "Adicionar Curso"
    Foram adicionadas as funções novo e salvar no controlador de cursos.
    A função "Create" é responsável por retornar a visão responsavel pelo formulário de cadastro de curso.
    A função "Store" é responsável por alocar uma instancia do modelo "Curso", configurar seus atributos de maneira que corresponda ao que foi escrito no formulário e em seguida redirecionar para  a página do formulário para que o usuário possa cadastrar outro curso (se desejar).

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class CursosController extends Controller
 {
-    //
+  public function create(){
+    //Retorna a view referente ao formulario
+    return view('curso.formulario');
+  }
+
+  public function store(Request $request){
+    $curso = new Curso();
+    $cursos->create($request->all());
+
+    //Mensagem de aviso: curso cadastrado
+    //\Session::flash('mensagem_sucesso_curso','Curso cadastrado com sucesso');
+
+    //Redireciona para a pagina de formualario de curso para caso o usuário
+    //queira criar outro curso, nela deve haver um botão retorno para caso o
+    //usuário queira voltar para uma certa pagina
+    return redirect->back();
+  }
 }
