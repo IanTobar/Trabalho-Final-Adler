42ce846ea6c604076090e028b85d0b967ebdea94
Arthur Ferreira Leão Chagas <arthurfleao@gmail.com>
2017-05-27 20:57:15 -0300

    Preparação para a associação do design da tela de login

--- resources/views/cursos/formulario.blade.php
+++ resources/views/cursos/formulario.blade.php
@@ -1,5 +1,6 @@
+@extends(layouts.app)
 
-
+@section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -34,3 +35,4 @@
         </div>
     </div>
 </div>
+@endsection
