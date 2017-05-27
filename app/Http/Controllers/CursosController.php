949b1478df742da30734afad709c3efe405bd2fb
Unknown <ruanbento@gmail.com>
2017-05-27 19:16:50 -0300

    Retirada de comentarios

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -14,10 +14,7 @@ class CursosController extends Controller
   public function store(Request $request){
     $curso = new Curso();
     $cursos->create($request->all());
-
-    //Mensagem de aviso: curso cadastrado
-    //\Session::flash('mensagem_sucesso_curso','Curso cadastrado com sucesso');
-
+    
     //Redireciona para a pagina de formualario de curso para caso o usuário
     //queira criar outro curso, nela deve haver um botão retorno para caso o
     //usuário queira voltar para uma certa pagina
