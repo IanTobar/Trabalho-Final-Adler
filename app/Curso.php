ac19204f64b4ec2507f4e4a8c6ddb8c3b8dc5a2c
Ian Marcelo Tobar <ianeloi@hotmail.com>
2017-05-27 19:15:13 -0300

    Alterado modelo de Curso e Disciplina
    Adicionado os campos fillable

--- app/Curso.php
+++ app/Curso.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model
 {
-    //
+    protected $fillable ={
+      'nomeCurso';
+      'cargaHoraria';
+      'tamanhoTurma';
+      'duracao';
+      'cordenadorCurso';
+    }
 }
