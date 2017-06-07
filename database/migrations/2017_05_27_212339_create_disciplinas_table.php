3ab61abcf112809b1d75d50939898f3130410f90
Ian Marcelo Tobar <ianeloi@hotmail.com>
2017-06-07 20:37:56 -0300

    Alterado para uso de chave estrangeira

--- database/migrations/2017_05_27_212339_create_disciplinas_table.php
+++ database/migrations/2017_05_27_212339_create_disciplinas_table.php
@@ -17,6 +17,8 @@ class CreateDisciplinasTable extends Migration
             $table->increments('id');
             $table->string('nomeDisciplina');
             $table->integer('cargaHoraria');
+            $table->integer('curso_id')->unsigned();
+            $table->foreign('curso_id')->references('id')->on('cursos');
             $table->timestamps();
         });
     }
